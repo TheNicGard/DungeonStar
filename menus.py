eb415db066ca6ad7015ccf67a6a8f59c2502c6de
Nicolas Gard <nicolasg699@gmail.com>
2019-07-10 18:28:29 -0500

    updated title screen

--- menus.py
+++ menus.py
@@ -43,7 +43,7 @@ def main_menu(con, background_image, screen_width, screen_height):
     libtcod.console_set_default_foreground(0, libtcod.light_yellow)
     libtcod.console_print_ex(0, int(screen_width / 2), int(screen_height / 2) - 4,
                              libtcod.BKGND_NONE, libtcod.CENTER,
-                             'TOMBS OF THE ANCIENT KINGS')
+                             'Dungeon Star')
     libtcod.console_print_ex(0, int(screen_width / 2), int(screen_height - 2),
                              libtcod.BKGND_NONE, libtcod.CENTER, 'Nic Gard (C) 2019')
 
