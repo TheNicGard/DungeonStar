c2511905a22f1c43eb5228b244d063bf2241aff8
Nicolas Gard <nicolasg699@gmail.com>
2019-08-25 03:46:31 -0500

    added game_container for persistent non-save information

---
+++ game_container.py
@@ -0,0 +1,7 @@
+
+class GameContainer:
+    def __init__(self, lowest_level, high_score, stat_diffs, points_available):
+        self.lowest_level = lowest_level
+        self.high_score = high_score
+        self.stat_diffs = stat_diffs
+        self.points_available = points_available
